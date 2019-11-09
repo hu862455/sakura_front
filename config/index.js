07c9f54905295c948ca3db160380c037e86a1fd0
hushuai <hellohushuai@gmail.com>
2019-11-09 17:37:56 +0800

    完成随机测试部分代码

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/sakura': { //这里最好有一个 /
-        target: 'http://10.4.132.165:8088', // 后台接口域名
+        target: 'http://192.168.2.6:8088', // 后台接口域名
         ws: true, //如果要代理 websockets，配置这个参数
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, //是否跨域
